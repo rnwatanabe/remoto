f5542dad9d6d58607e82af940921b7e06c65af65
rnwatanabe <renato.navillewatanabe@gmail.com>
2015-06-25 18:24:34 -0300

    interneuron slope correction

--- remoto/src/br/remoto/model/Neuron/Interneuron.java
+++ remoto/src/br/remoto/model/Neuron/Interneuron.java
@@ -57,10 +57,10 @@ public class Interneuron extends SpinalNeuron
 	{
 		try
 		{
-			double k1 = dVs_dt( 1, t,									saturate( vs ) );
+			double k1 = dVs_dt( 2, t,									saturate( vs ) );
 			double k2 = dVs_dt( 2, t + miscellaneous.getStepByTwo(),	saturate( vs + miscellaneous.getStepByTwo() * k1 ) );
-			double k3 = dVs_dt( 3, t + miscellaneous.getStepByTwo(),	saturate( vs + miscellaneous.getStepByTwo() * k2 ) );
-			double k4 = dVs_dt( 4, t + miscellaneous.getStep(),			saturate( vs + miscellaneous.getStep() * k3 ) );
+			double k3 = dVs_dt( 2, t + miscellaneous.getStepByTwo(),	saturate( vs + miscellaneous.getStepByTwo() * k2 ) );
+			double k4 = dVs_dt( 2, t + miscellaneous.getStep(),			saturate( vs + miscellaneous.getStep() * k3 ) );
 	
 			vs = saturate( vs + miscellaneous.getStepBySix() * (k1 + 2*k2 + 2*k3 + k4) );
 
